f8d70452c1d26156794fadc97bb2ed6b5fdb9664
Rohith <rohith.97.rk@gmail.com>
2023-04-10 13:53:50 +0530

    secretkey

--- shoezyy/shoezyy/settings.py
+++ shoezyy/shoezyy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jb@do!p54q+#@svz0o5c77b17qv+a!4(#s2%j-mlb-smpb2)=x'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
